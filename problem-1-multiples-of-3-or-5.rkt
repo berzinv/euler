ec7df59c3453f5c3be9425fca0ef2674e12dec5d
Vincent BERZIN <berzinv@gmail.com>
2022-11-12 10:48:07 +0100

    Problem 1: Multiples of 3 or 5

---
+++ problem-1-multiples-of-3-or-5.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (filter-range fct cnt)
+  (filter fct (range 1 cnt)))
+
+(define (multiple-of-3-or-5 x)
+  (if (or (zero? (modulo x 3))
+          (zero? (modulo x 5)))
+      x
+      false))
+
+(define (sum lst)
+  (apply + lst))
+
+(sum (filter-range multiple-of-3-or-5 10))
+
